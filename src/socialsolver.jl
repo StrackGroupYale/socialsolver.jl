20674250eb3426165fbc3f93f7472a97a3b61809
Academic and Research <AnR@Joshuas-MacBook-Pro.local>
2020-04-22 11:57:35 -0400

    update

--- src/socialsolver.jl
+++ src/socialsolver.jl
@@ -1,5 +1,44 @@
+__precompile__()
+
 module socialsolver
 
-greet() = print("Hello World!")
+    # stdlib (standard libraries)
+    using Dates
+    using Distributed
+    using LinearAlgebra # for LowerTriangular, logdet, diag
+    using Printf: @printf, @sprintf
+    using Random
+    using Statistics: mean, quantile, median
 
-end # module
+    # other libraries, indicate compatible version in Project.toml
+    using CSV
+    using DataFrames
+    using DelimitedFiles
+    using JuMP
+    using Cbc
+    using GLPK
+    using JLD
+    using MathOptInterface
+    using Distributions
+
+    import Base: show
+    import GLM: ftest
+
+    const DEBUGC = false # even more debug messages
+    global CHECKNET = false # for debugging only
+
+    export ftest
+    export
+        ## Network Definition
+        problem_glpk, 
+        direct_glpk, 
+        time_test, 
+        welfare,
+        mech_basic_cbc,
+        data_gen,
+        data_gen_direct
+
+    include("solve.jl")
+    include("gen.jl")
+    include("x_t.jl")
+end #module
